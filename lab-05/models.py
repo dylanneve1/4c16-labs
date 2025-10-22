9addd3d2063ea86815d340e3579780d0da51eaf8
Dylan Robert Neve <neved@tcd.ie>
2025-10-22 12:46:28 +0000

    Committing Lab 05 for running tests

--- lab-05/models.py
+++ lab-05/models.py
@@ -25,21 +25,22 @@ class ConvNN(nn.Module):
         	nn.ReLU(inplace=True),
         	nn.MaxPool2d(kernel_size=2, stride=2),
         	# Block 4
-        	nn.Conv2d(256, 512, kernel_size=3, padding=1),
+        	nn.Conv2d(256, 256, kernel_size=3, padding=1),
         	nn.ReLU(inplace=True),
-        	nn.Conv2d(512, 512, kernel_size=3, padding=1),
+        	nn.Conv2d(256, 256, kernel_size=3, padding=1),
         	nn.ReLU(inplace=True),
         	nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.avgpool = nn.AdaptiveAvgPool2d((2, 2))
         self.classifier = nn.Sequential(
           nn.Dropout(0.5),
-        	nn.Linear(512 * 2 * 2, 1024),
+        	nn.Linear(256 * 2 * 2, 1024),
         	nn.ReLU(inplace=True),
           nn.Dropout(0.5),
-			    nn.Linear(1024, 512),
+			    nn.Linear(1024, 256),
         	nn.ReLU(inplace=True),
-        	nn.Linear(512, num_classes),
+          nn.Dropout(0.3),
+        	nn.Linear(256, num_classes),
         )
 
 
