29d6484f56fd651e918de213190ac46e43421ebe
Dylan Robert Neve <neved@tcd.ie>
2025-10-22 09:10:52 +0000

    Committing Lab 05 for running tests

---
+++ lab-05/models.py
@@ -0,0 +1,15 @@
+import torch                                      # The main PyTorch library
+import torch.nn as nn                             # Contains pytorch network building blocks (e.g., layers)
+import torch.nn.functional as F                   # Contains functions for neural network operations (e.g., activation functions)
+
+class ConvNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        # We are not providing any example.
+        # look back at lab 4 and add convolutional layers
+
+    def forward(self, x):
+        # same here, look back at lab 4.
+        
+        return x
