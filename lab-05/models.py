c1be895fddaf1ccc4dcaac85c56fa359481639ed
Dylan Robert Neve <neved@tcd.ie>
2025-10-22 12:38:01 +0000

    Committing Lab 05 for running tests

--- lab-05/models.py
+++ lab-05/models.py
@@ -4,12 +4,51 @@ import torch.nn.functional as F                   # Contains functions for neura
 
 class ConvNN(nn.Module):
     def __init__(self, num_classes=10):
-        super().__init__()
+        super(ConvNN, self).__init__()
+        self.features = nn.Sequential(
+        	# Block 1
+        	nn.Conv2d(3, 64, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.Conv2d(64, 64, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.MaxPool2d(kernel_size=2, stride=2),
+        	# Block 2
+        	nn.Conv2d(64, 128, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.Conv2d(128, 128, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.MaxPool2d(kernel_size=2, stride=2),
+        	# Block 3
+        	nn.Conv2d(128, 256, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.Conv2d(256, 256, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.MaxPool2d(kernel_size=2, stride=2),
+        	# Block 4
+        	nn.Conv2d(256, 512, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.Conv2d(512, 512, kernel_size=3, padding=1),
+        	nn.ReLU(inplace=True),
+        	nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((2, 2))
+        self.classifier = nn.Sequential(
+          nn.Dropout(0.5),
+        	nn.Linear(512 * 2 * 2, 1024),
+        	nn.ReLU(inplace=True),
+          nn.Dropout(0.5),
+			    nn.Linear(1024, 512),
+        	nn.ReLU(inplace=True),
+        	nn.Linear(512, num_classes),
+        )
 
-        # We are not providing any example.
-        # look back at lab 4 and add convolutional layers
 
     def forward(self, x):
         # same here, look back at lab 4.
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
         
         return x
+
